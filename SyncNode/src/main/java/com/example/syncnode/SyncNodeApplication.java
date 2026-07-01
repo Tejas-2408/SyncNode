ac655741d138a46cb5cd6788141a1a31679a8325
Tejas Bansal <tejasbansal6065@gmail.com>
2026-07-02 01:25:25 +0530

    start

---
+++ SyncNode/src/main/java/com/example/syncnode/SyncNodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncNodeApplication.class, args);
+	}
+
+}
---
+++ SyncNode/src/test/java/com/example/syncnode/SyncNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
